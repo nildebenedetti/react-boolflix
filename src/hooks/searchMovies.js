ca5f2929db71397d114d3a1d47f9b1edf885e7bb
“Nilde <nilde.benedetti@gmail.com>
2026-05-18 17:01:15 +0200

    creato hook di ricerca per movies

---
+++ src/hooks/searchMovies.js
@@ -0,0 +1,24 @@
+
+function searchMovies(searchKeyword) {
+    
+    const API_KEY = import.meta.env.VITE_MOVIEDB_API_KEY;
+    // Compongo l'URL di ricerca
+    const url = `https://api.themoviedb.org/3/search/movie?query=${searchKeyword}&language=it-IT`;
+
+
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${ API_KEY }`
+        }
+    })
+    .then(response => {
+                if (response.status === 404) {
+                    throw new Error('Pagina non trovata')
+                }
+                return response.json();
+            })
+};
+
+export default searchMovies;
