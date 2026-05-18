919789856312d94917d3902bb90eb2de4d8e8609
“Nilde <nilde.benedetti@gmail.com>
2026-05-18 21:43:48 +0200

    fix: remove importazione flag icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'glafg-icons/css/glag-incons.min.css';
+
 
 // Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
 // Bootstrap JS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
 import './styles/index.css'
 
 
