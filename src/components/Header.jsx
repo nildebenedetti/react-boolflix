36fc44016bad6787f4297c81f2e0eda67772b9c5
“Nilde <nilde.benedetti@gmail.com>
2026-05-18 17:37:58 +0200

    add: form navbar con scheletro funzione handler

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
 import { NavLink } from "react-router";
 import useTheme from "../hooks/useTheme";
+import useAppData from "../hooks/useAppData";
+import { useState } from "react";
 
 function Header() {
     const { theme, toggleTheme } = useTheme();
+    const [ userInput, setUserInput ] = useState('');
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log('sono la handler!!')
+    };
 
     return (
         <header>
@@ -36,6 +44,14 @@ function Header() {
                             </li>
                         </ul>
                     </div>
+                </div>
+            </nav>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid d-flex justify-content-center">
+                    <form onSubmit={submitHandler} className="d-flex" role="search">
+                        <input className="form-control me-2" type="text" placeholder="Search"/>
+                        <button className="btn btn-outline-success" type="submit" >Search</button>
+                    </form>
                 </div>
             </nav>
         </header>
