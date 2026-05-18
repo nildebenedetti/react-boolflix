ba84014968a0251da02994e576e938cd8310ddd2
“Nilde <nilde.benedetti@gmail.com>
2026-05-18 18:44:35 +0200

    add: map nel main per visualizzzione dati testuali di MovieList

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,30 @@ import useAppData from "../hooks/useAppData";
 function Main() {
     const { moviesList } = useAppData();
 
-  return (
-    <div>
-        <p>{JSON.stringify(moviesList)}</p>
-    </div>
-  )
+    return <>
+        <div>
+            <ul>
+                {moviesList.map((movie) => {
+                    const {
+                        id,
+                        name,
+                        original_name: orName,         
+                        original_language: orLanguage,  
+                        vote_average: rating          
+                    } = movie;
+
+                    return (
+                        <li key={id}>
+                            <h3>{`Titolo: ${name}`}</h3>
+                            <h5>{`Titolo Originale: ${orName}`}</h5> {/* Ora puoi usare orName! */}
+                            <p>{`Lingua Originale: ${orLanguage}`}</p>
+                            <p>{`Rating: ${rating}/10`}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    </>
+
 }
 export default Main;
