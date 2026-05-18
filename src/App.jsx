98c44448046e7df721ce42a52a11f32cc65797c3
“Nilde <nilde.benedetti@gmail.com>
2026-05-18 15:43:47 +0200

    add: wrap con AppDataProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { AppDataProvider } from "./contexts/AppDataContext";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 
 function App() {
+
+  
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <AppDataProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </AppDataProvider>
   );
 }
 export default App;
