39074d0c6c1b4dcbee32af24b078bccd583450be
“Nilde <nilde.benedetti@gmail.com>
2026-05-19 16:49:54 +0200

    fix: formatting + add overview section in card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import { getImgURL } from "../utils/tmdb";
 
 
 
-function Card({ title, orTitle, id, orLanguage, rating, flagCode, posterPath, category }) {
+function Card({ title, orTitle, id, orLanguage, rating, flagCode, overview, posterPath, category }) {
 
   const backdrop_sizes = [
     "w342",
@@ -14,32 +14,35 @@ function Card({ title, orTitle, id, orLanguage, rating, flagCode, posterPath, ca
   return <>
     <li>
       <div className="flipcard-container">
-      <div className="flip-card p-3 m-2">
-      <div className="card-front">
-        <img
-          src={getImgURL(posterPath, backdrop_sizes[0])} // estrarlo, normalizzarlo in context e pssarlo
-          className="card-img-top"
-          alt={`Poster di ${title}`}
-        />
-      </div>
-      <div className="card-back">
-        <div className="card-body d-flex flex-column justify-content-between">
-          <div>
-            <h5 className="card-subtitle mb-2 text-muted h6">Titolo Originale: {orTitle}     <span className={`fi fi-${flagCode} rounded-1`}></span></h5>
+        <div className="flip-card p-3 m-2">
+          <div className="card-front">
+            <img
+              src={getImgURL(posterPath, backdrop_sizes[0])} // estrarlo, normalizzarlo in context e pssarlo
+              className="card-img-top"
+              alt={`Poster di ${title}`}
+            />
           </div>
-          <div>
-            <h3 className="card-title">{title}</h3>
-          </div>
-          <div className="d-flex align-items-center gap-2 mt-3">
-            <p className="card-text mb-0 fw-bold">
-              Rating: {parseFloat(rating.toFixed(1))}/10
-            </p>
+          <div className="card-back">
+            <div className="card-body">
+              <div className="py-3">
+                <h2 className="card-title">{title}</h2>
+              </div>
+              <div className="py-3">
+                <h5 className="original-title-text card-subtitle mb-2 text-muted h5">Titolo Originale: <br></br> {orTitle}   <br></br>  <span className={`fi fi-${flagCode} rounded-1`}></span></h5>
+              </div>
+              <div className="py-3">
+                <p className="card-text mb-0 fw-bold">
+                  Rating: {parseFloat(rating.toFixed(1))}/10
+                </p>
+                </div>
+                <div className="py-3">
+                <p className="overview-text">{overview}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-    </div>
-  </li >
+    </li >
   </>
 }
 
