243d070ab6c594aeb66cd622f54537b44cd4009e
“Nilde <nilde.benedetti@gmail.com>
2026-05-19 14:33:34 +0200

    remove: log di controllo

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -2,13 +2,11 @@ import useAppData from "../hooks/useAppData";
 import ResultSection from "../components/ResultSection";
 
 function Series() {
-    const { mashedResults, setMashedResults } = useAppData();
-    console.log("Tutti i risultati nel Context:", mashedResults);
+    const { mashedResults } = useAppData();
+    
     const mashedResultsFilteredSeries = mashedResults.filter( (result => {
         return result.category === 'series';
     }));
-    console.log("i risultati post filter", mashedResultsFilteredSeries);
-    
 
     return <>
         <h1> Serie TV</h1>
