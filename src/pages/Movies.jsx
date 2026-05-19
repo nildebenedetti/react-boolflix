c11ffe8d221b9c2a3ae0beddee9df2c50838b288
“Nilde <nilde.benedetti@gmail.com>
2026-05-19 14:33:18 +0200

    fix: function in movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,15 +1,19 @@
 import useAppData from "../hooks/useAppData";
 import ResultSection from "../components/ResultSection";
 
-function Series() {
-    const { standardMoviesList } = useAppData();
+function Movies() {
+    const { mashedResults } = useAppData();
+
+    const mashedResultsFilteredMovies = mashedResults.filter((result => {
+        return result.category === 'movie';
+    }));
 
     return <>
         <h1> Film</h1>
         {/* qui dopo ci metto le serie popolari */}
         <section className="search-results-movies">
-            <ResultSection items={standardSeriesList} />
+            <ResultSection items={mashedResultsFilteredMovies} />
         </section>
     </>
 }
-export default Series;
+export default Movies;
