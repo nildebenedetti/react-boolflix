1dbc41cfe45205a4cbe5503057510cdff1fdbf29
“Nilde <nilde.benedetti@gmail.com>
2026-05-19 14:26:00 +0200

    add: filter per mostrare solo risultati con ricerca serie

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -2,13 +2,19 @@ import useAppData from "../hooks/useAppData";
 import ResultSection from "../components/ResultSection";
 
 function Series() {
-    const { standardSeriesList } = useAppData();
+    const { mashedResults, setMashedResults } = useAppData();
+    console.log("Tutti i risultati nel Context:", mashedResults);
+    const mashedResultsFilteredSeries = mashedResults.filter( (result => {
+        return result.category === 'series';
+    }));
+    console.log("i risultati post filter", mashedResultsFilteredSeries);
+    
 
     return <>
         <h1> Serie TV</h1>
         {/* qui dopo ci metto le serie popolari */}
         <section className="search-results-series">
-            <ResultSection items={standardSeriesList} />
+            <ResultSection items={mashedResultsFilteredSeries} />
         </section>
     </>
 }
